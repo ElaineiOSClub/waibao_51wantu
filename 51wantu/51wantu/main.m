fac183964c703344552848db974fdf711eb6eb5c
ElaineDXL <248301344@qq.com>
2015-08-08 10:34:28 +0800

    xx

---
+++ 51wantu/51wantu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  51wantu
+//
+//  Created by elaine on 15/8/8.
+//  Copyright (c) 2015年 elaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
