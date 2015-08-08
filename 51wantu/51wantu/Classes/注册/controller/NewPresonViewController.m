25fba81832f0ff91a9babf1547878f16038b994d
kevin <75423338@qq.com>
2015-08-09 00:25:04 +0800

    更新了一些小东西

--- 51wantu/51wantu/Classes/注册/controller/NewPresonViewController.m
+++ 51wantu/51wantu/Classes/注册/controller/NewPresonViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "NewPresonViewController.h"
+#import "Util.h"
+#import "MBProgressHUD+MJ.h"
 
 @interface NewPresonViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *emailTextF;
@@ -33,6 +35,32 @@
     [self.emailTextF becomeFirstResponder];
 
 }
+
+- (void)signUpMethod
+{
+    [self.view endEditing:YES];
+    
+    if (self.emailTextF.text == nil || [self.emailTextF.text isEqualToString:@"" ])
+    {
+        [Util showAlertWithTitle:@"提示" msg:@"邮箱不能为空"];
+        
+        return;
+    }
+    
+    if (!IS_AVAILABLE_EMAIL(self.emailTextF.text)) {
+        [Util showAlertWithTitle:@"提示"  msg:@"请输入正确的邮箱地址"];
+        return;
+    }
+    
+    if (self.pwdTextF.text.length < 6) {
+        [Util showAlertWithTitle:@"提示" msg:@"密码长度不能少于6个"];
+        return;
+    }
+    [MBProgressHUD showMessage:@"注册中,请稍等..."];
+    
+//    [self signUpRequestHttpToServerWithpsWord:self.pwdText.text];
+}
+
 
 /*
 #pragma mark - Navigation
